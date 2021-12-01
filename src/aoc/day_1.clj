bc1c56fed3a31447f304afcde81cfcd5f2842be9
Brandon Olivier <brandon@brandonolivier.com>
2021-11-30 23:50:56 -0600

    Add solution 2

--- src/aoc/day_1.clj
+++ src/aoc/day_1.clj
@@ -17,3 +17,30 @@
        (filter (fn [[prev next]]
                  (< prev next)))
        count))
+
+(defn sum [coll]
+  (apply + coll))
+
+(defn solution-2 [input]
+  (->> input
+       (partition 3 1)
+       (map sum)
+       (partition 2 1)
+       (filter (fn [[prev next]]
+                 (< prev next)))
+       count))
+
+(comment
+  (def input [199
+              200
+              208
+              210
+              200
+              207
+              240
+              269
+              260
+              263])
+
+  (def input (read-integer-file-lines "resources/day_1.input"))
+  )
